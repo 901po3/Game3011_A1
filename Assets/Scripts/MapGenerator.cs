5f24da7f4d0c4ca3387b3536feeb39d487b11a75
Hyukin Kown <901po3@gmail.com>
2020-01-14 18:16:00 -0500

    1

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    const int MAP_SIZE = 16;
+    [SerializeField] GameObject tile;
+
+    private void Start()
+    {
+        if(tile != null)
+        {
+            float tileSize = tile.transform.localScale.x;
+
+            for(int i = 0; i < MAP_SIZE; i++)
+            {
+                for(int j = 0; j < MAP_SIZE; j++)
+                {
+
+                }
+            }
+        }     
+    }
+}
