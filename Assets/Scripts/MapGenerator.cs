392126234e872ee3959f3a7aafdd0cee40bae4f2
Hyukin Kown <901po3@gmail.com>
2020-01-15 14:38:36 -0500

    made tile and cam movement

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,20 +6,30 @@ public class MapGenerator : MonoBehaviour
 {
     const int MAP_SIZE = 16;
     [SerializeField] GameObject tile;
+    GameObject[,] tileMap = new GameObject[MAP_SIZE, MAP_SIZE];
 
     private void Start()
     {
-        if(tile != null)
+        if (tile != null)
         {
-            float tileSize = tile.transform.localScale.x;
+            float tileSize = tile.transform.localScale.x + tile.transform.localScale.x / 8;
 
             for(int i = 0; i < MAP_SIZE; i++)
             {
                 for(int j = 0; j < MAP_SIZE; j++)
                 {
-
+                    GameObject newTile = Instantiate(tile, transform);
+                    newTile.transform.position = new Vector3(j * tileSize, 0, i * tileSize);
+                    tileMap[j, i] = newTile;
                 }
             }
         }     
+    }
+
+
+
+    void MakeResourceSet(int x, int y, float value)
+    {
+        //tileMap[x, y];
     }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    const float MINIMAL_VALUE = 200;
+    public enum Type {Maximum, Half, Quarter, Minimal };
+    Type type = Type.Minimal;
+    [SerializeField] Material[] materials;
+
+    public float value;
+
+    private void Start()
+    {
+        SetValueByType(MINIMAL_VALUE);
+    }
+
+    public void TypeChanger(Type newtype)
+    {
+        type = newtype;
+    }
+
+    public void SetValueByType(float newValue)
+    {
+        switch(type)
+        {
+            case Type.Maximum:
+                value = newValue;
+                gameObject.GetComponent<Renderer>().material = materials[0];
+                break;
+            case Type.Half:
+                value = newValue / 2;
+                gameObject.GetComponent<Renderer>().material = materials[1];
+                break;
+            case Type.Quarter:
+                value = newValue / 4;
+                gameObject.GetComponent<Renderer>().material = materials[2];
+                break;
+            case Type.Minimal:
+                value = MINIMAL_VALUE;
+                gameObject.GetComponent<Renderer>().material = materials[3];
+                break;
+            default:
+                break;
+        }
+    }
+}
